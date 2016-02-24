2700b96b07994155a04281eabb1d5a51cd60836c
Marius Berge Eide <eide@mpa-garching.mpg.de>
2016-02-24 09:51:14 +0100

    Coniguration file

---
+++ cci-sort/config.py
@@ -0,0 +1,14 @@
+
+CCI_USER    = 'anonymous'
+CCI_PASS    = 'guest'
+CCI_FTP     = 'anon-ftp.ceda.ac.uk'
+#CCI_HOME    = '/neodc/esacci/'
+#CCI_HOME    = '/neodc/esacci/aerosol/data/AATSR_SU/L3_DAILY/v4.2/2002/'
+CCI_HOME    = '/neodc/esacci/fire/data/burned_area/grid/v3.1/2006/'
+CCI_DTYPE   = 'nc'
+
+CCI_API_URL  = 'http://localhost:5000/'
+CCI_API_EXP  = CCI_API_URL + 'experiments/'
+CCI_API_PROD = CCI_API_URL + 'products/'
+CCI_API_FILS = CCI_API_URL + 'files/'
+
